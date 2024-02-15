3ea1efe6fd31e9f1e405c44f90c17d000d7f5e2f
ybuiko <juliabuiko@gmail.com>
2024-02-15 15:40:08 -0500

    added read me

--- bookmarks_fe/src/App.js
+++ bookmarks_fe/src/App.js
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <Outlet />
     </div>
   );
