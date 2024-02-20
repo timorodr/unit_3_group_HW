b416b0e8caebacf38344a4468c2fbd87bc505a9f
Roman Larionov <pcsaveddotcom@gmail.com>
2024-02-19 23:28:02 -0500

    Styling applied

--- bookmarks_fe/src/App.js
+++ bookmarks_fe/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
--- bookmarks_fe/src/loaders.js
+++ bookmarks_fe/src/loaders.js
@@ -5,9 +5,3 @@ export const bookmarksLoader = async () => {
     const bookmarks = await response.json()
     return bookmarks
 }
-
-// export const bookmarkLoader = async ({params}) => {
-//     const response = await fetch(`${URL}/bookmarks/${params.id}`) // ALWAYS MAKE SURE THIS ROUTE MATCHES HOW DEFINED IN BE
-//     const bookmark = await response.json()
-//     return bookmark
-// }
